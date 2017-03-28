7bc05ce34435634cbfecf80719f16abde444aa3a
Bob McAteer <bob@robertmcateer.com>
2017-03-28 16:42:40 +0100

    Updating add event stuff

--- Navigation/app/src/main/java/com/robertmcateer/com/navigation/AddEvent.java
+++ Navigation/app/src/main/java/com/robertmcateer/com/navigation/AddEvent.java
@@ -16,9 +16,14 @@ import android.widget.Toast;
 
 public class AddEvent extends Fragment implements DatePickerDialog.OnDateSetListener, View.OnClickListener {
 
-    EditText title, venue, url, type;
-    TextView date;
+    EditText TITLE_TEXT, VENUE_TEXT, URL_TEXT, TYPE_TEXT;
+    TextView DATE_TEXT;
     Button dateBtn, addBtn;
+
+    Calendar c = Calendar.getInstance();
+    int mYear = c.get(Calendar.YEAR);
+    int mMonth = c.get(Calendar.MONTH);
+    int mDay = c.get(Calendar.DAY_OF_MONTH);
 
     public AddEvent() {}
 
@@ -29,67 +34,18 @@ public class AddEvent extends Fragment implements DatePickerDialog.OnDateSetList
 
         View v = inflater.inflate(R.layout.fragment_add_event, container, false);
 
-        title = (EditText)v.findViewById(R.id.AddEventTitle);
-        date = (TextView) v.findViewById(R.id.AddDateText);
-        venue = (EditText)v.findViewById(R.id.AddVenueEvent);
-        type = (EditText)v.findViewById(R.id.AddTypeEvent);
-        url = (EditText)v.findViewById(R.id.AddURLEvent);
+        TITLE_TEXT = (EditText)v.findViewById(R.id.AddEventTitle);
+        DATE_TEXT = (TextView) v.findViewById(R.id.AddDateText);
+        VENUE_TEXT = (EditText)v.findViewById(R.id.AddVenueEvent);
+        TYPE_TEXT = (EditText)v.findViewById(R.id.AddTypeEvent);
+        URL_TEXT = (EditText)v.findViewById(R.id.AddURLEvent);
 
         dateBtn = (Button)v.findViewById(R.id.AddDateEvent);
         addBtn = (Button)v.findViewById(R.id.AddEventButton);
-        int i = date.length();
-
-        addBtn.setOnClickListener(new View.OnClickListener()
-        {
-            // I know this is not a fantastic way to do it but it works really well :)
-            public void onClick(View v)
-            {
-                boolean cleanedFields = validateFields(new EditText[]{title, url, venue, type});
-
-                if(cleanedFields)
-                {
-                    if(date.length() != 0)
-                    {
-                        // Add a new event once all fields are populated
-                        addNewEvent(
-                                title.getText().toString(),
-                                date.getText().toString(),
-                                venue.getText().toString(),
-                                type.getText().toString(),
-                                url.getText().toString());
-                        // Prints to the screen to show completion of adding
-                        Toast toast = Toast.makeText(
-                                getActivity().getApplicationContext(),
-                                "Your event is added!",
-                                Toast.LENGTH_LONG
-                        );
-                        toast.show();
-                    }
-                    else
-                    {
-                        // When the date field is empty
-                        Toast toast = Toast.makeText(
-                                getActivity().getApplicationContext(),
-                                "Ahh! You are missing a date. Please select one",
-                                Toast.LENGTH_LONG);
-                        toast.show();
-                    }
-                }
-                else
-                {
-                    // When something other than the date field is missing!
-                    Toast.makeText(getActivity().getApplicationContext(),
-                            "Oh No! You're missing a field. Please check them all!",
-                            Toast.LENGTH_LONG)
-                            .show();
-                }
 
 
 
-
-            }
-        });
-
+        addBtn.setOnClickListener(this);
         dateBtn.setOnClickListener(this);
 
         return v;
@@ -118,20 +74,70 @@ public class AddEvent extends Fragment implements DatePickerDialog.OnDateSetList
     private void addNewEvent(String title, String date, String venue, String type, String url)
     {
         DatabaseHandler db = new DatabaseHandler(getActivity().getApplicationContext());
-        db.addEvent(new NewEvent(title, url, venue, date, type));
+        boolean cleanedFields = validateFields(new EditText[]{TITLE_TEXT, URL_TEXT, VENUE_TEXT, TYPE_TEXT});
+
+        if(cleanedFields)
+        {
+            if(DATE_TEXT.length() != 0)
+            {
+                // Add a new event once all fields are populated
+                db.addEvent(new NewEvent(title, url, venue, date, type));
+
+                // Prints to the screen to show completion of adding
+                Toast toast = Toast.makeText(
+                        getActivity().getApplicationContext(),
+                        "Your event is added!",
+                        Toast.LENGTH_LONG
+                );
+                toast.show();
+            }
+            else
+            {
+                // When the date field is empty
+                Toast toast = Toast.makeText(
+                        getActivity().getApplicationContext(),
+                        "Ahh! You are missing a date. Please select one",
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+        else
+        {
+            // When something other than the date field is missing!
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Oh No! You're missing a field. Please check them all!",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
     {
-        Log.i("CALENDAR", "DATE SET");
-        date.setText(year+"/"+month+"/"+dayOfMonth);
+        DATE_TEXT.setText(year+"-"+month+"-"+dayOfMonth);
     }
 
     @Override
     public void onClick(View v)
     {
-        DatePickerDialog dialog = new DatePickerDialog(getContext(), this, 2017, 3, 26);
-        dialog.show();
+        switch(v.getId())
+        {
+            case R.id.AddDateEvent:
+                DatePickerDialog dialog = new DatePickerDialog(getContext(), this, mYear, mMonth, mDay);
+                dialog.show();
+                break;
+
+            case R.id.AddEventButton:
+                addNewEvent(
+                        TITLE_TEXT.toString(),
+                        URL_TEXT.toString(),
+                        VENUE_TEXT.toString(),
+                        TYPE_TEXT.toString(),
+                        URL_TEXT.toString());
+                break;
+
+            default:
+                break;
+        }
     }
 }
